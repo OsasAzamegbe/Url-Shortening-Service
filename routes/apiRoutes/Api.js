91d1b3bc233e9ff7a87b11efeb7f36e3bad252aa
osamudiamen <osas.azamegbe@gmail.com>
2020-10-10 19:40:00 +0100

    removed register and login routes from Api.js

--- routes/apiRoutes/Api.js
+++ routes/apiRoutes/Api.js
@@ -2,16 +2,11 @@ const router = require('express').Router()
 
 const shortenUrlRoute = require('./ShortenUrl')
 const addKeysRoute = require('./AddKeys')
-const registerRoute = require('./Register')
-const loginRoute = require('./Login')
 const validate = require('../../validationService/ValidateToken')
 
 
 router.use('/shorten', validate, shortenUrlRoute)
 router.use('/addkeys', addKeysRoute)
-router.use('/register', registerRoute)
-router.use('/login', loginRoute)
-
 
 
 module.exports = router
