2fc3f95aab8d00af374f473c124686c47cd41c6d
osamudiamen <osas.azamegbe@gmail.com>
2020-10-10 11:17:33 +0100

    created User model

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        max: 20
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 100,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 8,
+        max: 2048
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User
