65c0121121ce3c59bbd1fb3239958d516a70fa63
osamudiamen <osas.azamegbe@gmail.com>
2020-10-10 13:18:21 +0100

    hooked up Register and Login routes to Api

--- routes/apiRoutes/Api.js
+++ routes/apiRoutes/Api.js
@@ -2,11 +2,15 @@ const router = require('express').Router()
 
 const shortenUrlRoute = require('./ShortenUrl')
 const addKeysRoute = require('./AddKeys')
+const registerRoute = require('./Register')
+const loginRoute = require('./Login')
 
 
 
 router.use('/shorten', shortenUrlRoute)
 router.use('/addkeys', addKeysRoute)
+router.use('/register', registerRoute)
+router.use('/login', loginRoute)
 
 
 
