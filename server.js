49b5bb1e2e160e6eca783c75e2d620b8be94ab6d
osamudiamen <osas.azamegbe@gmail.com>
2020-10-12 14:38:09 +0100

    enabled cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const apiRoute = require('./routes/apiRoutes/Api')
 const redirectRoute = require('./routes/Redirect')
@@ -40,6 +41,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //middleware
+app.use(cors())
 app.use(bodyParser.json())
 
 //routes
